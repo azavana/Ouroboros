76d0bb09434d576075d5bb7089f3a57b209a0407
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2018-11-05 21:14:03 +0300

    Add files via upload

---
+++ Ouroboros/Core.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_h_
+#define HEADER_h_
+
+int GCD (int a, int b);
+
+void swap (unsigned char *a, unsigned char *b);
+
+unsigned char *swap_character (unsigned char *text);
+
+void KSA (char *key, unsigned char S[256]);
+
+void PRG (unsigned char S1[256], unsigned char S2[256], unsigned char *plaintext, unsigned char *ciphertext);
+
+void OUROBOROS_Encrypt (unsigned char *plaintext, char *key, unsigned char *ciphertext);
+
+unsigned char *OUROBOROS_Key();
+
+
+#endif
